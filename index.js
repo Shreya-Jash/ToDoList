ec889b50f52f8ed7d71b5c605f5133cb2a82bf38
Shreya Jash <shreyajash98@gmail.com>
2022-08-03 18:08:34 +0530

    update

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
+let saveTask=[]
+
+const inputTask=document.getElementById("input-task")
 let inputBtn=document.getElementById("input-btn")
-
-
-
+const lisT=document.getElementById("list")
 
 inputBtn.addEventListener("click",function(){
-
+        if (inputTask.value === '') {
+            alert("Please enter a task")
+        }
+        else{
+            saveTask.push(inputTask.value)
+            renderTask()
+            inputTask.value = ""
+        } 
 })
+
+function renderTask(){
+            //let listItems= ""
+            const li =document.createElement("li")
+            const closebtn=document.createElement("Button")
+            closebtn.innerHTML="X"
+            //add a list item
+            for(let i=0;i<saveTask.length;i++)
+            {
+               // listItems+= "<li>" + saveTask[i] + "</li> "
+             
+                li.textContent=saveTask[i]
+                li.appendChild(closebtn) //add button to list
+                lisT.appendChild(li)
+            }
+            //delete the complete list on button click
+            closebtn.addEventListener("click",function(){
+                li.style.display="none"
+             })
+            //lisT.innerHTML=listItems   
+}
